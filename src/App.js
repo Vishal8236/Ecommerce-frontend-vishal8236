23be55be5b6a15cfc28a0849eb7708df005a807c
vishal8236 <vishalpatidar963027@gmail.com>
2021-08-06 16:54:17 +0530

    add react router,axios,bootstrap package and setup router in app.js file

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import './App.css';
 import React, { useEffect, useState } from "react"
 import Home from './components/Home';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Route} from 'react-router-dom';
+import { BrowserRouter as Router,Switch,Route,Link } from "react-router-dom";
 import Login from './components/session/Login';
 import axios from 'axios';
 import Dashboard from './components/Dashboard/Dashboard';
-import Navigation from './components/Navigation.jsx/Navigation';
+import Navigation from './components/Navigation/Navigation';
 import Signup from './components/session/Signup';
 
 function App() {
@@ -18,6 +18,7 @@ function App() {
 				headers: {"Authenticate" : localStorage.token}
 			})
 			.then(res => {
+        console.log(res)
 				setuser(res.data)
 			})
 		}	
@@ -25,7 +26,9 @@ function App() {
 
   return (
     <div className="App">
-          {user.user ?  <Navigation user={user.user['name']} /> : <Navigation user="user" /> }
+      <Router>
+        {user.user ?  <Navigation user={user.user['name']} /> : <Navigation user="user" /> }
+        <Switch>
           <Route exact path="/" >
             <Home />
           </Route>
@@ -38,6 +41,8 @@ function App() {
           <Route path="/dashboard">
             <Dashboard />
           </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
